cafc1d086ef6b75c7e28e6eae92c33cb734b3801
Katy <kn96@njit.edu>
2016-11-07 21:50:31 -0500

    array_pop

--- arraysite.php
+++ arraysite.php
@@ -50,6 +50,13 @@
 ?>
 
 <html> <h3> array_pop </h3> </html>
+
+<?php
+  $stack = array("orange", "banana", "apple", "raspberry");
+  $fruit = array_pop($stack);
+  print_r($stack);
+?>
+
 <html> <h3> array_push </h3> </html>
 <html> <h3> array_combine </h3> </html>
 <html> <h3> fgetcsv </h3> </html>
