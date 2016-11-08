a80222785e5211624508518f89bf057bffc68dec
Katy <kn96@njit.edu>
2016-11-07 21:36:14 -0500

    array_walk

--- arraysite.php
+++ arraysite.php
@@ -26,6 +26,29 @@
 ?>
 
 <html> <h3> array_walk </h3> </html>
+
+<?php
+  $fruits = array("d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple");
+  
+  function test_alter(&$item1, $key, $prefix)
+  {
+      $item1 = "$prefix: $item1";
+  }
+  
+  function test_print($item2, $key)
+  {
+      echo "$key. $item2<br />\n";
+  }
+  
+  echo "Before ...:\n";
+  array_walk($fruits, 'test_print');
+  
+  array_walk($fruits, 'test_alter', 'fruit');
+  echo "... and after:\n";
+  
+  array_walk($fruits, 'test_print');
+?>
+
 <html> <h3> array_pop </h3> </html>
 <html> <h3> array_push </h3> </html>
 <html> <h3> array_combine </h3> </html>
