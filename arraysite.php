a805a484628b37637d485d5c9796ee7bbd3a2bf4
Katy <kn96@njit.edu>
2016-11-07 19:14:54 -0500

    array_search

--- arraysite.php
+++ arraysite.php
@@ -14,8 +14,17 @@
   print_r(array_keys($array));
 ?>
 
-
 <html> <h3> array_search </h3> </html>
+
+<?php
+  $array = array(0 => 'blue', 1 => 'red', 2 => 'green', 3 => 'red');
+  
+  $key = array_search('green', $array); // $key = 2;
+  echo $key;
+  $key = array_search('red', $array);   // $key = 1;
+  echo $key;
+?>
+
 <html> <h3> array_walk </h3> </html>
 <html> <h3> array_pop </h3> </html>
 <html> <h3> array_push </h3> </html>
